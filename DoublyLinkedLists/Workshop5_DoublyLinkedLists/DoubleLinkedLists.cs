bbba5a37fb933e332bfd8700ffb5a9f36c28dea2
Jeison Cordoba <jeisonsmit@gmail.com>
2025-10-12 11:42:45 -0500

    Creation of printing methods, display list at the beginning and at the end, Node and Console

---
+++ DoublyLinkedLists/Workshop5_DoublyLinkedLists/DoubleLinkedLists.cs
@@ -0,0 +1,76 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using System.Drawing;
+
+
+namespace Workshop5_DoublyLinkedLists.Cor;
+
+public class DoublyLinkedLists<T>
+{
+    private DoubleNode<T>? _tail;
+    private DoubleNode<T>? _head;
+
+    public DoublyLinkedLists()
+    {
+        _tail = null;
+        _head = null;
+    }
+
+    public void InsertAtBeginning(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        if (_head == null)
+        {
+            _head = newNode; //--> Pointer
+            _tail = newNode; //--> Pointer
+        }
+        else
+        {
+            newNode.Next = _head;
+            _head.Prev = newNode;
+            _head = newNode;
+        }
+    }
+    public void InsertAtEnd(T data)
+    {
+        var newNode = new DoubleNode<T>(data);
+        {
+            if (_tail == null)
+            {
+                _head = newNode; //--> Pointer
+                _tail = newNode; //--> Pointer
+            }
+            else
+            {
+                newNode.Prev = _tail;
+                _tail.Next = newNode;
+                _tail = newNode;
+            }
+        }
+    }
+    
+    public string GetForward()
+    {    
+        var output = string.Empty;
+        var current = _head;
+
+        while (current != null)
+        {
+            output += ($"{current.Data} <--> ");
+            current = current.Next;
+        }
+        return output. Substring(0, output.Length -5);
+    }
+
+    public string GetBackward()
+    {
+        var output = string.Empty;
+        var current = _tail;
+
+        while (current != null)
+        {
+            output += ($"{current.Data} <--> ");
+            current = current.Prev;
+        }
+        return output.Substring(0, output.Length -5);
+    }
+}
---
+++ DoublyLinkedLists/Workshop5_DoublyLinkedLists/DoubleNode.cs
@@ -0,0 +1,17 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Workshop5_DoublyLinkedLists.Cor;
+
+public class DoubleNode<T>
+{
+    public DoubleNode(T data) //Constructor whit pointers
+    {
+        Data = data;
+        Next = null;
+        Prev = null;
+    }
+        public DoubleNode<T>? Prev { get; set; }
+        public T? Data { get; set; }
+        public DoubleNode<T>? Next { get; set; }    
+}
