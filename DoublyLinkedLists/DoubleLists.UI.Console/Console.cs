696aeec5568471406820448e2712d00a7244a58f
Jeison Cordoba <jeisonsmit@gmail.com>
2025-10-12 11:41:44 -0500

    Creation of printing methods, display list at the beginning and at the end, Node and Console.

---
+++ DoublyLinkedLists/DoubleLists.UI.Console/Console.cs
@@ -0,0 +1,9 @@
+﻿using Workshop5_DoublyLinkedLists.Cor;
+
+
+var list = new DoublyLinkedLists<string>();
+list.InsertAtBeginning("Perro");
+list.InsertAtBeginning("Gato");
+list.InsertAtEnd("Loro");
+Console.WriteLine(list.GetForward());
+Console.WriteLine(list.GetBackward());
